b6e21f782c0ab65f8c639454d13ffb2d77ec27dd
Eric H Martanto <codium@MacBook-Pro-4.local>
2020-08-19 14:33:14 +0700

    WIP: not finished yet, because still has issues run test

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
-//
-// Your implementations
-// 
+export function updateGameScore(player1Points, player2Points): Game {
+    const game = new Game
+    game.player1Points = player1Points
+    game.player2Points = player2Points
+    return game
+}
+
+export function decideGameWinner(game): string {
+    const player1Points = game.player1Points
+    const player2Points = game.player1Points
+
+    if (player1Points >= 4 && player1Points - player2Points >= 2) {
+        return 'Player 1'
+    } 
+    if (player2Points >= 4 && player2Points - player1Points >= 2) {
+        return 'Player 2'
+    }
+}
+
+export function printScore([player1Points, player2Points]): string {
+    const scoreArray = ['love', 'fifteen', 'thirty', 'forty']
+    if (player1Points == 3 && player2Points == 3) {
+        return 'deuce'
+    }
+}
+
+class Game {
+    public player1Points: number;
+    public player2Points: number;
+}
