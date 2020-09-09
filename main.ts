c7943d8b238681c5f481a29e670a1add7c9e06c3
Eric H Martanto <codium@90.191-100-1.etapanet.net>
2020-09-09 13:50:16 +0700

    refactor function

--- main.ts
+++ main.ts
@@ -3,9 +3,6 @@ class Game {
     public player2Points: number;
 }
 
-
-
-
 export function updateGameScore(player1Points: number, player2Points: number): Game {
     const game = new Game
     game.player1Points = player1Points
@@ -13,27 +10,51 @@ export function updateGameScore(player1Points: number, player2Points: number): G
     return game
 }
 
+function player1WillWin(player1Points: number, player2Points: number) {
+    return player1Points>= 4 && player1Points - player2Points >= 2
+}
+
+function player2WillWin(player1Points: number, player2Points: number) {
+    return player2Points >= 4 && player2Points - player1Points >= 2
+}
+
+function bothMoreThan3Points(player1Points: number, player2Points: number) {
+    return player1Points >= 3 && player2Points >= 3
+}
+
+function willDeuce(player1Points: number, player2Points: number) {
+    return player1Points == player2Points
+}
+
+function advantage1(player1Points: number, player2Points: number) {
+    return player1Points == player2Points + 1
+}
+
+function advantage2(player1Points: number, player2Points: number) {
+    return player2Points == player1Points + 1
+}
+
 export function decideGameWinner(game): string {
     const player1Points = game.player1Points
     const player2Points = game.player2Points
-    if (player1Points >= 4 && player1Points - player2Points >= 2) {
+    if (player1WillWin) {
         return 'Player 1'
     } 
-    if (player2Points >= 4 && player2Points - player1Points >= 2) {
+    if (player2WillWin) {
         return 'Player 2'
     }
 }
 
 export function printScore([player1Points, player2Points]): string {
     const scoreArray = ['love', 'fifteen', 'thirty', 'forty']
-    if (player1Points >= 3 && player2Points >= 3) {
-        if (player1Points == player2Points) {
+    if (bothMoreThan3Points) {
+        if (willDeuce) {
             return 'deuce'
         }
-        if (player1Points == player2Points + 1) {
+        if (advantage1) {
             return 'advantage player 1'
         }
-        if (player2Points == player1Points + 1) {
+        if (advantage2) {
             return 'advantage player 2'
         }   
     }
